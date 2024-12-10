65d20d2f4f405caf898c22935dd3ac7753df7e4b
Hampus <hampus@hampushuledal.com>
2024-12-10 18:34:26 +0100

    Update python.py
    add day 6 problem 1 solution

--- python.py
+++ python.py
@@ -1,53 +1,86 @@
 
-rules: list[tuple[int,int]] = []
-updates: list[list[int]] = []
+class Guard:
+	x: int
+	y: int
+	dir: int
+	steps: set[tuple]
+	def __init__(self, x, y, dir:str):
+		self.x = x
+		self.y = y
+		match dir:
+			case "^":
+				self.dir = 0
+			case ">":
+				self.dir = 1
+			case "v":
+				self.dir = 2
+			case "<":
+				self.dir = 3
+			case _:
+				raise Exception(f"invalid symbol for direction:{dir}")
+		
+		self.steps = set([self.get_pos()])
+				
+	def get_next(self) -> tuple[int, int]:
+		match self.dir:
+			case 0:
+				return (self.x, self.y-1)
+			case 1:
+				return (self.x+1, self.y)
+			case 2:
+				return (self.x, self.y+1)
+			case 3:
+				return (self.x-1,self.y)
+			case _:
+				raise Exception(f"invalid direction {self.dir}")
+		
+	def turn(self):
+		self.dir = (self.dir + 1) % 4
+		
+	def step(self):
+		(self.x, self.y) = self.get_next()
+		self.steps.add(self.get_pos())
+		
+	def is_on_map(self, map_width, map_height) -> bool:
+		return 0 <= self.x < map_width and 0 <= self.y < map_height
+		
+	def get_pos(self) -> tuple[int,int]:
+		return (self.x,self.y)
+	
+	def get_steps(self) -> int:
+		return len(self.steps)
+		
+
+obstacles: set[tuple[int,int]] = set()
+guard: Guard
+
+width: int
+height: int
 
 with open('data.txt', 'r') as file:
-		parsing_rules = True
-		
-		for line in file:
-			if line == "\n":
-				parsing_rules = False
-				continue
-			
-			if parsing_rules:
-				split = line.split('|')
-				rules.append((int(split[0]),int(split[1])))
+			y = 0
+			lines = file.readlines()
+			height = len(lines)
+			width = len(lines[0].strip())
+			for line in lines:
+				x= 0
+				for char in line.strip():
+					match char:
+						case "#":
+							obstacles.add((x,y))
+						case ">" | "v" | "<" | "^":
+							guard = Guard(x,y,char)
+						case ".":
+							pass
+						case _:
+							raise Exception("invalid charachter in input: {char}")
+					x += 1
+				y+=1
+
+while guard.is_on_map(width, height):
+			if guard.get_next() in obstacles:
+				guard.turn()
 			else:
-				split = line.split(',')
-				update = []
-				for entry in split:
-					update.append(int(entry))
-				updates.append(update)
+				guard.step()
 
-result = 0
-to_be_sorted: list[list[int]] = []
-print(to_be_sorted)
-		
-for update in updates:
-	for (page_1, page_2) in rules:
-		if page_1 not in update or page_2 not in update:
-			continue
-		
-		if update.index(page_1) > update.index(page_2):
-			to_be_sorted.append(update)
-			break
-			
-			
-for update in to_be_sorted:
-	swapped = True
-	while swapped:
-		swapped = False
-		for (page_1,page_2) in rules:
-			if page_1 not in update or page_2 not in update:
-				continue
-			
-			if update.index(page_1) > update.index(page_2):
-				update.remove(page_1)
-				update.insert(update.index(page_2),page_1)
-				swapped = True
-
-for update in to_be_sorted:
-	result += update[int(len(update)/2)]
-
-print(result)
+print(guard.get_steps()-1)
