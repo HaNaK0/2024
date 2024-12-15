555f29a05d2af50af5a7b2e14872513d85771cb5
Hampus <hampus@hampushuledal.com>
2024-12-15 14:20:04 +0100

    add day 11 part 2
    this works fast enough

--- python.py
+++ python.py
@@ -1,44 +1,46 @@
-import concurrent.futures
-
 def blink_one(stone: int) -> list[int]:
-	if stone == 0:
-		return [1]
-		
-	s_str = str(stone)
-	if len(s_str) % 2 == 0:
-		half = int(len(s_str)/ 2)
-		return [int(s_str[:half]), int(s_str[half:])]
-	
-	return [stone * 2024]
- 	
-def blink(stones: list[int])-> list[int]:
-	out = list()
-	for stone in stones:
-		stone_str = str(stone)
-		if stone == 0:
-			out.append(1)
-		elif len(stone_str) % 2 == 0:
-			half = int(len(stone_str) / 2)
-			out.append(int(stone_str[:half]))
-			out.append(int(stone_str[half:]))
-		else:
-			out.append(stone * 2024)
-			
-	return out
-			
+        if stone == 0:
+                return [1]
 
-stones: list[int] 
+        s_str = str(stone)
+        if len(s_str) % 2 == 0:
+                half = int(len(s_str)/ 2)
+                return [int(s_str[:half]), int(s_str[half:])]
+
+        return [stone * 2024]
+
+def blink(stones: list[int])-> list[int]:
+        out = list()
+        for stone in stones:
+                stone_str = str(stone)
+                if stone == 0:
+                        out.append(1)
+                elif len(stone_str) % 2 == 0:
+                        half = int(len(stone_str) / 2)
+                        out.append(int(stone_str[:half]))
+                        out.append(int(stone_str[half:]))
+                else:
+                        out.append(stone * 2024)
+
+        return out
+
+stones: dict[int:int] = {}
 
 with open("data.txt", "r") as file:
-	line = file.readline().strip()
-	stones = [int(c) for c in line.split(" ")]
+        line = file.readline().strip()
+        stones = {int(c):1 for c in line.split(" ")}
 
-
-with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-	for i in range(0,75):
-		result = list()
-		for out in executor.map(blink_one, stones):
-			result.extend(out)
-		stones = result
-		print(i)
-	print(len(stones))
+for i in range(0,75):
+	new_stones = dict()
+	for stone, amount in stones.items():
+		for s in blink_one(stone):
+			if s in new_stones:
+				new_stones[s] += amount
+			else:
+				new_stones[s] = amount
+				
+	stones = new_stones
+					
+print(sum(stones.values()))
+with open("out.txt", "w") as file:
+	file.write(str(sum(stones.values())))
